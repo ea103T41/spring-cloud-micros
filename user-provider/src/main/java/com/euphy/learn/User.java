560f3e50cdfcb2675deee3d9222a92dbd13616d2
euphyfang <ea103t41@gmail.com>
2024-12-31 14:35:30 +0800

    fix: avoid manually inserting IDs and ensure ids are automatically generated by the database.

--- user-provider/src/main/java/com/euphy/learn/User.java
+++ user-provider/src/main/java/com/euphy/learn/User.java
@@ -2,6 +2,7 @@ package com.euphy.learn;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     String name;
     String email;
