8a0af4097f1627af61007cf7992cba6a13bdaf5f
euphyfang <ea103t41@gmail.com>
2025-01-09 17:51:39 +0800

    feat: implement config server and set client config

---
+++ config-server/src/main/java/com/euphy/learn/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.euphy.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+    System.out.println("http://localhost:8888");
+  }
+}
